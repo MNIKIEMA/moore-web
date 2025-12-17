f2185995be2ecec9b999429a647cc91a11f4569c
MNIKIEMA <nikiemamahamadi01@gmail.com>
2025-12-17 22:17:26 +0100

    add book parser for bilingual book

---
+++ src/moore_web/book_parser.py
@@ -0,0 +1,32 @@
+import pymupdf
+
+
+def parse_book(doc: pymupdf.Document):
+    all_parts = []
+    for i, page in enumerate(doc, start=1):
+        page_width = page.rect.width
+        middle_x = page_width / 2
+        moore_parts = []
+        french_parts = []
+        if i < 3:
+            continue
+        if i > 6:
+            break
+        blocks = page.get_text("blocks", sort = True)
+        for b in blocks:
+            x0, y0, x1, y1, text = b[:5]
+            text = text.strip()
+
+            if not text:
+                continue
+            if text.isdigit():
+                continue
+
+            if x0 < middle_x:
+                moore_parts.append(text)
+            else:
+                french_parts.append(text)
+
+        all_parts.append({"moore": moore_parts, "french": french_parts, "page": i})
+
+    return all_parts
