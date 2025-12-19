da3e531a4daea1ae3bcdb465e0106b005ca3c201
MNIKIEMA <nikiemamahamadi01@gmail.com>
2025-12-19 10:30:57 +0100

    add to do in comment

--- src/moore_web/book_parser.py
+++ src/moore_web/book_parser.py
@@ -2,15 +2,16 @@ import pymupdf
 
 
 def parse_book(doc: pymupdf.Document):
+    # TODO: Find alignment automatically @critical
     all_parts = []
-    for i, page in enumerate(doc, start=1):
+    for i, page in enumerate(doc, start=1):  #type: ignore
         page_width = page.rect.width
         middle_x = page_width / 2
         moore_parts = []
         french_parts = []
         if i < 3:
             continue
-        if i > 6:
+        if i > 47:
             break
         blocks = page.get_text("blocks", sort = True)
         for b in blocks:
