5f5f1e8838fc98db143dd1c4dcd1cd567a00fae4
MNIKIEMA <nikiemamahamadi01@gmail.com>
2026-01-11 11:58:09 +0100

    add chapter delimiter

---
+++ src/moore_web/constants.py
@@ -0,0 +1,111 @@
+CHAPTERS = [
+    (
+        r"""Chapitre 1 
+ 
+Les secrets de Maman""",
+        r"""Sak a 1 soaba 
+ 
+M ma solga bũmbu""",
+    ),
+    (
+        r"""Chapitre 2
+Davantage de pro-
+blèmes dans la famille  
+de Poko""",
+        r"""Sak a 2 soaba
+Yɛla ket n paasda       
+a Pok rãmb zak n wã""",
+    ),
+    (
+        r"""Chapitre 3
+Des dangers pour
+la famille
+de Poko""",
+        r"""Sak a 3 soaba  
+ 
+A Pok rãmb  
+zaka  
+zu-loeese""",
+    ),
+    (
+        r"""Chapitre 4
+Poko retrouve  
+espoir""",
+        r"""Sak a 4 soaba  
+A Pok ne a yaopa  
+tõog n gesa b meng 
+yelle""",
+    ),
+    (
+        r"""Chapitre 5  
+La communauté de 
+Poko s’informe sur 
+le SIDA""",
+        r"""Sak a 5 soaba 
+ A Pok rãmb tẽnga 
+rãmb baome n na n 
+bãng sẽn kẽed SIDAwã 
+bãag wɛɛngẽ. """,
+    ),
+]
+
+CHAPTER_FIVE_ENUM = [
+    (
+        r"""1. Qu'est-ce que le SIDA    
+et comment cause-t-il la 
+mort ?""",
+        r"""1. SIDAwã bãag ya 
+boẽ? La a maanda a 
+wãn n kʋʋd nebã?""",
+        "",
+    ),
+    (
+        r"""2. Comment une per-
+sonne""",
+        r"""2. SIDAwã bãag maanda 
+       a wãn n yõk neda?""",
+        "attrape-t-elle le VIH ?",
+    ),
+    (
+        r"""3. Comment une personne 
+peut-elle savoir si elle a 
+attrapé le VIH ?""",
+        r"""3. Ned na n maana a wãn n 
+bãng tɩ SIDAwã bãag tar-a 
+lame?""",
+        "",
+    ),
+    (
+        r"""4. Quels sont quelques-
+uns des signes qui peu-
+vent indiquer qu’une per-
+sonne peut avoir le SIDA ?""",
+        r"""4. Boẽ ne boẽ n wiligd tɩ
+ned tara SIDAwã bãagã?""",
+        "",
+    ),
+    (
+        r"""5. Y a-t-il des médica-
+ments qu’une per-
+sonne atteinte du SI-
+DA peut prendre 
+pour se sentir""",
+        r"""5. Tɩɩm beeme tɩ a soab 
+ninga SIDAwã bãag sẽn 
+tara wã toẽ n dɩkdẽ tɩ 
+sãõog bii?""",
+        "mieux ?",
+    ),
+    (
+        r"""6. Nous connaissons 
+une personne qui a le 
+SIDA. Elle va bientôt 
+mourir. Comment      
+l’aider ?""",
+        r"""6. Tõnd sã n mi ned 
+SIDAwã bãag sẽn tar-a, tɩ 
+d mii t‘a na n kiimi; d na 
+n maana a wãn n sõng-a?""",
+        "",
+    ),
+]
