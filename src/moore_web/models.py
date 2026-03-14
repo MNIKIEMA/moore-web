d2d1e0dfd058e68f1cf78156c96ec82b1ea88ca6
MNIKIEMA <nikiemamahamadi01@gmail.com>
2026-03-14 22:58:44 +0100

    Add a data structure for dict output

---
+++ src/moore_web/models.py
@@ -0,0 +1,26 @@
+import msgspec
+
+
+class Example(msgspec.Struct):
+    moore: str
+    french: str
+    english: str | None = None
+
+
+class Sense(msgspec.Struct):
+    id: str
+    french: str
+    english: str
+    examples: list[Example] = []
+    category: str | None = None
+    scientific_name: str | None = None
+    synonym: str | None = None
+    antonym: str | None = None
+
+
+class DictionaryEntry(msgspec.Struct):
+    lemma: str
+    ipa: str
+    pos: str
+    senses: list[Sense]
+    variants: dict[str, list[str]] | None = None
